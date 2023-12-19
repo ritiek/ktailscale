697e38049d0cff51a70c52cb6ab7e9798b2a6214
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2023-12-19 17:53:50 +0530

    Some tuning

--- ktailscale/bin/tailscale.sh
+++ ktailscale/bin/tailscale.sh
@@ -3,14 +3,14 @@
 _KH_FUNCS="/mnt/us/usbnet/bin/libkh5"
 . "${_KH_FUNCS}"
 
-KH_HACKNAME="${PWD##/mnt/us/extensions/}"
-EXTENSION=/mnt/us/extensions/${KH_HACKNAME}
+KH_HACKNAME="ktailscale"
+EXTENSION="/mnt/us/extensions/${KH_HACKNAME}"
 
 
 start() {
   kh_msg "* starting tailscaled *" I v
   ${EXTENSION}/bin/tailscaled --cleanup
-  ${EXTENSION}/bin/tailscaled --tun=userspace-networking &
+  ${EXTENSION}/bin/tailscaled --tun=userspace-networking --socks5-server=127.0.0.1:1055 --outbound-http-proxy-listen=127.0.0.1:1055 --no-logs-no-support &
   kh_msg "* starting tailscale *" I v
   ${EXTENSION}/bin/tailscale up --accept-dns=true
   kh_msg "* started tailscale *" I v
@@ -20,19 +20,20 @@ stop() {
   kh_msg "* stopping tailscale *" I v
   ${EXTENSION}/bin/tailscale down
   ${EXTENSION}/bin/tailscaled --cleanup
+  pkill -f tailscaled
   kh_msg "* stopped tailscale *" I v
 }
 
 
 ## Main
 case "${1}" in
-        "start" )
-                ${1}
-        ;;
-        "stop" )
-                ${1}
-        ;;
-        * )
-                kh_msg "invalid action (${1})" W v "invalid action"
-        ;;
+  "start" )
+    ${1}
+  ;;
+   "stop" )
+    ${1}
+  ;;
+    * )
+    kh_msg "invalid action (${1})" W v "invalid action"
+  ;;
 esac
